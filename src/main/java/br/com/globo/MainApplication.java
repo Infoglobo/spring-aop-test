39937d618a3657f0d631c31fc5980d395bb6151f
Willian Antunes <willian.lima.antunes@gmail.com>
2018-08-11 12:59:35 -0300

    Details to make it run in an IDE

--- src/main/java/br/com/globo/MainApplication.java
+++ src/main/java/br/com/globo/MainApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
+    /**
+     * Quando rodar via IDE use os seguintes parâmetros:
+     * <ul>
+     *     <li>--add-opens java.base/java.lang=spring.core</li>
+     *     <li>--add-opens java.base/java.io=tomcat.embed.core</li>
+     * </ul>
+     */
     public static void main(String[] args) {
 
         SpringApplication.run(MainApplication.class, args);
